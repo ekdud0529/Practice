154ad03af4040a3dc2eaf0fb88392c990abe6a6c
dayoung <ekffkddl0529@naver.com>
2021-04-20 22:07:38 +0900

    kcbert1

--- BERT/torchPt.py
+++ BERT/torchPt.py
@@ -1,4 +1,30 @@
-import torch
+from transformers import BertTokenizer
+tokenizer = BertTokenizer.from_pretrained(
+    "beomi/kcbert-base",
+    do_lower_case=False,
+)
 
-x = torch.Tensor([[1, 2], [3, 4]])
-print(x)
+from transformers import BertConfig, BertModel
+pretrained_model_config = BertConfig.from_pretrained(
+    "beomi/kcbert-base"
+)
+
+import torch
+model = BertModel.from_pretrained(
+    "beomi/kcbert-base",
+    config=pretrained_model_config,
+)
+
+sentences = ["안녕하세요", "하이!"]
+features = tokenizer(
+    sentences,
+    max_length=10,
+    padding="max_length",
+    truncation=True,
+)
+
+features = {k: torch.tensor(v) for k, v in features.items()}
+
+outputs = model(**features)
+
+print(outputs[1])
