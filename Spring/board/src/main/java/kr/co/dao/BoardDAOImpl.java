10ccd0f143133fca96e352acfc1f543f1b6cc15a
ekdud0529 <ekffkddl0529@naver.com>
2021-12-11 18:51:13 +0900

    add

--- Spring/board/src/main/java/kr/co/dao/BoardDAOImpl.java
+++ Spring/board/src/main/java/kr/co/dao/BoardDAOImpl.java
@@ -1,5 +1,7 @@
 package kr.co.dao;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -16,5 +18,11 @@ public class BoardDAOImpl implements BoardDAO{
 	@Override
 	public void write(BoardVO boardVO) throws Exception{
 		sqlSession.insert("boardMapper.insert", boardVO);
+	}
+	
+	// 게시물 목록 조회
+	@Override
+	public List<BoardVO> list() throws Exception {	
+		return sqlSession.selectList("boardMapper.list");
 	}
 }
