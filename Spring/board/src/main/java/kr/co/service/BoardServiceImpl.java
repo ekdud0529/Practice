0683232e27e96dc5f23c82d3a2140bfa505c8c9b
ekdud0529 <ekffkddl0529@naver.com>
2021-12-14 19:28:28 +0900

    add modify and delete

--- Spring/board/src/main/java/kr/co/service/BoardServiceImpl.java
+++ Spring/board/src/main/java/kr/co/service/BoardServiceImpl.java
@@ -25,4 +25,25 @@ public class BoardServiceImpl implements BoardService{
 	public List<BoardVO> list() throws Exception {
 			return dao.list();
 	}
+	
+	// 게시물 목록 조회
+	@Override
+	public BoardVO read(int bno) throws Exception {
+
+		return dao.read(bno);
+	}
+	
+	// 게시물 수정
+	@Override
+	public void update(BoardVO boardVO) throws Exception {
+
+		dao.update(boardVO);
+	}
+
+	// 게시물 삭제
+	@Override
+	public void delete(int bno) throws Exception {
+		
+		dao.delete(bno);
+	}
 }
